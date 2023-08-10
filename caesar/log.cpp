b883cd036941f28ca7a317d1e2efeabbfd269cb3
AcFerrer <60701881+lietlaw97@users.noreply.github.com>
2023-08-10 16:03:40 +0800

    Update log.cpp

--- caesar/log.cpp
+++ caesar/log.cpp
@@ -24,17 +24,16 @@ void inputLog() {
 
     else if (ans == "decrypt") {
         std::cout << "What mode would you like to use? bruteforce or normal mode? b/N: ";
-        std::cin.ignore();
         std::cin >> mode;
         char mode2 = tolower(mode);
-
+        // Clear the newline character from the buffer after reading mode
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         if (mode2 == 'b') {
             std::cout << "Enter ciphertext: ";
-            std::cin.ignore();
             std::getline(std::cin, text);
-         
             bruteForce(text);
         }
+
             
         else if (mode2 == 'n') {
             std::cout << "Enter ciphertext: ";
